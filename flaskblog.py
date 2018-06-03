002a5975f5b8a2c6b4ee59d4c4ed986e6e7c5980
Weilin Zou <weilinzou@gmail.com>
2018-06-03 13:33:55 -0400

    added css and register page etc

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '88602628d7c83008fa8b1ecd01691ba8'
+
+posts = [
+    {
+        'author': 'Weilin Zou',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '6/3/18'
+    },
+    {
+        'author': 'Not Me',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '6/3/18'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page<h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1> About Page <h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
